09a5bfea6b457390baa11d7f41d5fa9c9e2ba216
Asgeir Melling <asgmel@gmail.com>
2025-12-04 08:22:07 +0100

    fix: resolve type issue

--- grid/grid.go
+++ grid/grid.go
@@ -11,14 +11,14 @@ func (position Position) ToString() string {
 	return fmt.Sprintf("%v.%v", position.X, position.Y)
 }
 
-func (position Position) WithinBounds(matrix [][]any) bool {
+func WithinBounds[T any](position Position, matrix [][]T) bool {
 	if len(matrix) == 0 || len(matrix[0]) == 0 {
 		return false
 	}
 	return position.X >= 0 && position.X < len(matrix[0]) && position.Y >= 0 && position.Y < len(matrix)
 }
 
-func (position Position) GetNeighbouringPositions(matrix [][]any) []Position {
+func GetNeighbouringPositions[T any](position Position, matrix [][]T) []Position {
 	neighbours := []Position{}
 	for x := -1; x <= 1; x++ {
 		for y := -1; y <= 1; y++ {
@@ -26,7 +26,7 @@ func (position Position) GetNeighbouringPositions(matrix [][]any) []Position {
 				continue
 			}
 			currentPos := Position{X: position.X + x, Y: position.Y + y}
-			if currentPos.WithinBounds(matrix) {
+			if WithinBounds(currentPos, matrix) {
 				neighbours = append(neighbours, currentPos)
 			}
 		}
